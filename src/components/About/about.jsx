53b47d325bbbccccc1e31ab78c538a8b7bb22a98
servando18 <100006660+servando18@users.noreply.github.com>
2023-06-21 21:46:24 -0500

    update to about component

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import './about.css'
+import company from '../../assets/company.jpeg'
 
 const About = () => {
     return (
         <section id='about'>
             <h5>About Us</h5>
-        </section>
+
+            <div className="container about__container">
+        <div className="about__company">
+          <div className="about__company-image">
+            <img src={company} alt="About Image" />  
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+            
+              <h5>Experience</h5>
+              <small>10 Years in the business</small>
+            </article>
+
+            <article className='about__card'>
+            
+              <h5>Clients </h5>
+              <small>50+ clients</small>
+            </article>
+
+            <article className='about__card'>
+             
+              <h5>Projects</h5>
+              <small>100+ Completed</small>
+            </article>     
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quasi officia est odit ab doloribus.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Connect</a>
+        </div>
+      </div>
+    </section>
+    
     )
 }
 
